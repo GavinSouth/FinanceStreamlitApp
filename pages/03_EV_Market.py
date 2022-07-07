cd0d35462508ce6c7f334a8fa21e107bfd3e71df
GavinSouth <gavinsouth@msn.com>
2022-07-06 20:21:17 -0600

    July 6th, 2022

--- pages/03_EV_Market.py
+++ pages/03_EV_Market.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 st.markdown("# Comparing Available Electric Cars on the Market")
 
-st.markdown("The electric car market is starting be much more fleshed out. Most manufacturer conglomerates have at least one EV offering and many are on the verge of committing to an all electric range of vehicles. That being said below is a comparison of most of the current electric cars offered in 2022. Their cost and specs range vastly, but seeing the whole picture will help making a decision much easier.")
+st.markdown("The electric car market in 2022 is much more fleshed out than previous years. Most manufacturer conglomerates have at least one EV offering and many are on the verge of committing to an all electric range of vehicles. That being said below is a comparison of most of the current electric cars offered in 2022. Their cost and specs range vastly, but seeing the whole picture will help making a decision much easier.")
 
 car_data = pd.read_excel('Data/electric_car_market.xlsx')
 #st.dataframe(car_data.style.highlight_max(axis=0))
@@ -41,6 +41,8 @@ else:
     col3.metric("Range", 'TBD', delta_color="off")
 col4.metric("Type", str.upper(selected.iloc[0]['ev']), delta_color="off")
 
+st.markdown("The selected car from the slider is showing red on this chart to show a quick difference in specs to the other EVs on the market. To change the Y-axis variable use the radio button on the sidebar.")
+
 # ———————————————————————————————————————————————————————————————————————————————————————————————— #
 selection = st.sidebar.radio(
      "Choose Y Variable",
@@ -49,10 +51,13 @@ selection = st.sidebar.radio(
 Y = null
 if selection == 'Horse Power':
     Y = 'max_hp'
+    Y2 = 'max_hp_fuel'
 if selection == 'Range':
     Y = 'range_miles'
+    Y2 = 'range_miles_fuel'
 if selection == 'Fuel Economy':
     Y = 'fuel_economy'
+    Y2 = 'fuel_economy_fuel'
 
 def plot_specs(_Y):
     c = alt.Chart(car_data).encode(
@@ -74,8 +79,9 @@ plot_specs(Y)
 
 # ———————————————————————————————————————————————————————————————————————————————————————————————— #
 st.markdown("# Fuel Comparable Model")
-st.markdown("## " + selected.iloc[0]['Comparable_fuel_model'])
+st.markdown("To compare apples to apples as best as possible it seems like a useful comparison to look at vehicles that **cost the same as an electric model**. Seeing this comparison may make an even stronger case for the possible fuel savings over time. But, as you may see, this isn't the entire story. Some of the electric cars may have a lot less power and range compared to their gasoline cost equivilents. These trade-offs are variables to consider if you plan on going with an EV.")
 
+st.markdown("## " + selected.iloc[0]['Comparable_fuel_model'])
 st.image(Image.open('photos/car_photos/' + selected.iloc[0]['Comparable_fuel_model'] + '.jpeg'))
 
 col1, col2, col3, col4= st.columns(4)
@@ -95,3 +101,54 @@ if selected.iloc[0]['range_miles_fuel'] > 0 and selected.iloc[0]['range_miles'] 
 else:
     col3.metric("Range", 'TBD', delta_color="off")
 col4.metric("Fuel Type", str(selected.iloc[0]['fuel_type']), delta_color="off")
+
+st.markdown("You notice that some of the differences between the specs on the EV model and the fuel model reflect in color coding. This is an interesting distinction that will help you see the difference at a glimpse.")
+st.markdown("")
+
+def plot_specs(_Y2):
+    c = alt.Chart(car_data).encode(
+        x = alt.X('msrp_fuel', scale=alt.Scale(domain=[2e4, 12e4])),
+        y = alt.Y(_Y2),
+        tooltip=['Comparable_fuel_model', 'msrp_fuel', 'max_hp_fuel', 'range_miles_fuel']
+    ).properties(width=700, height=500)
+    c = c.mark_circle(color = 'lightgrey', size = 100) 
+    b = alt.Chart(selected).encode(
+        x = alt.X('msrp_fuel', scale=alt.Scale(domain=[2e4, 12e4])),
+        y = alt.Y(_Y2),
+        tooltip=['Comparable_fuel_model', 'msrp_fuel', 'max_hp_fuel', 'range_miles_fuel']
+    ).properties(width=700, height=500)
+    b = b.mark_circle(color = 'red', size = 200) 
+    d = c + b
+    d = d.configure_view(strokeWidth=0).configure_axis(grid=False, domain=False)
+    st.altair_chart(d, use_container_width=True)
+plot_specs(Y2)
+
+# ———————————————————————————————————————————————————————————————————————————————————————————————— #
+st.markdown("# Anticipated cost comparison:")
+st.markdown("#### Selected EV")
+col1, col2= st.columns(2)
+col1.metric("Lifetime Fuel Cost",
+            "$" +
+            round((((fuel_stats.avg_annual_miles / selected.iloc[0]['fuel_economy_fuel'])) * fuel_stats.current_premium) * fuel_stats.avg_length_of_ownership, 2).astype(str),
+            delta_color="off")
+col2.metric("Total Cost of Ownership", 
+             "$" + round((((fuel_stats.avg_annual_miles / selected.iloc[0]['fuel_economy_fuel'])) * fuel_stats.current_premium) * fuel_stats.avg_length_of_ownership + (selected.iloc[0]['msrp_fuel']), 2).astype(str),
+            delta_color="off")
+
+st.markdown("#### Selected Gas Equivilent")
+col1, col2= st.columns(2)
+col1.metric("Lifetime Fuel Cost",
+            "$" +
+            round((((fuel_stats.avg_annual_miles / selected.iloc[0]['fuel_economy_fuel'])) * fuel_stats.current_premium) * fuel_stats.avg_length_of_ownership, 2).astype(str),
+            delta_color="off")
+col2.metric("Total Cost of Ownership", 
+             "$" + round((((fuel_stats.avg_annual_miles / selected.iloc[0]['fuel_economy_fuel'])) * fuel_stats.current_premium) * fuel_stats.avg_length_of_ownership + (selected.iloc[0]['msrp_fuel']), 2).astype(str),
+            delta_color="off")
+
+
+
+
+
+
+
+# Further more within the electric car market some are even more fuel efficient than others.
---
+++ pages/04_Considerations.py
@@ -0,0 +1,26 @@
+# TO DO NEXT:
+# Convert kWh to the cost comparison
+# Talk about how some electric cars are much more efficient than others, especially the established ones.
+
+
+## Pros
+# charging at home
+# cost of maintenence and repairs
+# Economy: GMC Hummer is about at efficient as a Toyota Prius
+# Speed Torque
+# https://arstechnica.com/cars/2022/04/the-hummer-ev-is-an-electric-truck-for-people-who-think-evs-are-stupid/#:~:text=The%20official%20EPA%20efficiency%20figures,41%20kWh%2F100%20km).
+
+## Cons
+# Range anxiety
+# Cost and time to charge
+# Traveling long distances
+# Electric Charging stations.
+
+# There are some electric cars that are better than others due to some impressive tech. Tesla, GM.
+
+# Works:
+# https://www.fueleconomy.gov/feg/PowerSearch.do?action=noform&path=3&year1=2021&year2=2023&vtype=Electric&srchtyp=newAfv&pageno=3&rowLimit=50&sortBy=Comb&tabView=0
+# https://fred.stlouisfed.org/series/APU000072610
+# https://www.edmunds.com/fuel-economy/the-true-cost-of-powering-an-electric-car.html
+# https://arstechnica.com/cars/2022/04/the-hummer-ev-is-an-electric-truck-for-people-who-think-evs-are-stupid/#:~:text=The%20official%20EPA%20efficiency%20figures,41%20kWh%2F100%20km).
+# https://www.edmunds.com/car-news/tested-2022-audi-e-tron-gt-beats-epa-range-by-53-miles.html
